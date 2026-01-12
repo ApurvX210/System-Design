4a0e43dc4388cdc48a0afbd92a60c6e5ebe0ef15
ApurvX210 <apurvsharma210@gmail.com>
2026-01-12 22:38:51 +0530

    Implemented Elevator Abstraction

--- Elevator LLD/entities/Elevators.go
+++ Elevator LLD/entities/Elevators.go
@@ -3,9 +3,31 @@ package entities
 import "sync"
 
 type Elevator struct {
+	id				int
 	Buttons 		[]Pannel
 	mu      		sync.RWMutex
 	pos     		int
 	targetQueue		[]int
 	direction		bool
+}
+
+func (el *Elevator) TargetAdd(target int){
+	el.mu.Lock()
+	defer el.mu.Unlock()
+
+	el.targetQueue = append(el.targetQueue, target)
+}
+
+func (el *Elevator) GetELevatorId() int{
+	el.mu.RLocker()
+	defer el.mu.RUnlock()
+
+	return el.id
+}
+
+func (el *Elevator) GetPosAndTarget() (int,bool,[]int){
+	el.mu.RLocker()
+	defer el.mu.RUnlock()
+
+	return el.pos,el.direction,el.targetQueue
 }
